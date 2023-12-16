52082570f93754d91beca3d8be0f48d3b8b59335
Cameron Gilrein <Cameron.Gilrein@commscope.com>
2023-12-16 16:04:39 -0500

    Fixed sensor reading

--- voltage_test.py
+++ voltage_test.py
@@ -9,16 +9,21 @@ volts_channel = 1  # ADC channel for the voltage sensor on ADS1115
 
 def get_voltage():
     try:
-        print("Reading voltage sensor...")
+        # Read the analog value from the specified channel
         value = ads.read_adc(volts_channel, gain=1)
-        voltage = value / 32767.0 * 4.096  # Assuming gain=1 and VDD=4.096V
-        return round(voltage, 2)
-    except:
-        print("An error occurred while reading voltage sensor")
+        
+        # Convert the raw ADC value to voltage (assuming 5V reference voltage)
+        voltage = value * 0.000125  # 0.000125 V per unit for gain=1
+        
+        return voltage
+    except Exception as e:
+        print(f"An error occurred while reading voltage sensor: {e}")
         return None
 
-
 while True:
-    value = get_voltage()
-    print(value)
+    voltage_value = get_voltage()
+    
+    if voltage_value is not None:
+        print(f"Voltage: {voltage_value:.2f} V")
+    
     sleep(1)
