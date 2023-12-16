3f271389d880949dbf7cf0ee718a61892ed04344
Cameron Gilrein <Cameron.Gilrein@commscope.com>
2023-12-16 16:07:01 -0500

    Fixed sensor reading

--- voltage_test.py
+++ voltage_test.py
@@ -1,11 +1,17 @@
 from time import sleep
 from Adafruit_ADS1x15 import ADS1115
 
-ads = ADS1115()
-
-# Define the GPIO pin you want to re
-
+# Define the GPIO pin you want to read
 volts_channel = 1  # ADC channel for the voltage sensor on ADS1115
+
+def check_adc_connection():
+    try:
+        ads = ADS1115()
+        ads.read_adc(volts_channel, gain=1)
+        return True
+    except Exception as e:
+        print(f"ADC connection check failed: {e}")
+        return False
 
 def get_voltage():
     try:
@@ -19,6 +25,13 @@ def get_voltage():
     except Exception as e:
         print(f"An error occurred while reading voltage sensor: {e}")
         return None
+
+# Check ADC connection before entering the main loop
+if not check_adc_connection():
+    exit(1)
+
+# ADC is connected properly, proceed with the main loop
+ads = ADS1115()
 
 while True:
     voltage_value = get_voltage()
