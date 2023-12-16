664b312bfebc1bceb2b876996f128a12fdc494e1
Cameron Gilrein <Cameron.Gilrein@commscope.com>
2023-12-16 17:05:43 -0500

    Fixed sensor reading

--- new_voltage.py
+++ new_voltage.py
@@ -1,20 +1,17 @@
 import time
 import board
 import busio
-import adafruit_ads1x15.ADS1115 as ADS
+from Adafruit_ADS1x15 import ADS1115
 from adafruit_ads1x15.analog_in import AnalogIn
-
-
 
 i2c = busio.I2C(board.SCL, board.SDA)
 
-ads = ADS.ADS1115(i2c)
+ads = ADS1115(i2c)
 ads.gain = 1
 
-channel = AnalogIn(ads, ADS.P0)
+channel = AnalogIn(ads, ADS1115.P0)
 
 print("{:>5}\t{:>5}".format("raw","v"))
-
 
 while True:
     print("{:>5}\t{:>5.5f}".format(channel.value, channel.voltage))
