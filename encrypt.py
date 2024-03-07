34e2217376775851559dc252f8b0c12b9f61bc98
Cameron Gilrein <Cameron.Gilrein@commscope.com>
2024-03-07 17:32:50 -0500

    a

--- encrypt.py
+++ encrypt.py
@@ -14,7 +14,7 @@ def decrypt(encrypted_data):
 
 ### Put Tokens here ###
 token = ""
-encrypted_token = ""
+encrypted_token = "0d0703370725440a3c3f18381a25190a230c3426180216385c194a5e0006105f0820413f053b392e"
 
 if (input("Encrypting? y/n: ")):
     encrypted_token = encrypt(token)
