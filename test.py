71f00a4a1bebbbf0e8a35ab9f6f1aa48b1f2a1ae
Cameron Gilrein <Cameron.Gilrein@commscope.com>
2024-03-28 17:57:45 -0400

    Added cleanup

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(27,GPIO.OUT)
+GPIO.setup(36,GPIO.OUT)
 print ("LED on")
-GPIO.output(27,GPIO.HIGH)
+GPIO.output(36,GPIO.HIGH)
 time.sleep(1)
 print ("LED off")
-GPIO.output(27,GPIO.LOW)
+GPIO.output(36,GPIO.LOW)
